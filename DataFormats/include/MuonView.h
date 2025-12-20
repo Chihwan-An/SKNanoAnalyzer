dcb01dde7b58b9b4611d888afdffa299812a36f4
kyj519 <kyj519@snu.ac.kr>
2025-12-21 01:20:59 +0900

    intergrate muon and electron ID to one file for management

--- DataFormats/include/MuonView.h
+++ DataFormats/include/MuonView.h
@@ -8,6 +8,7 @@
 
 #include "TLorentzVector.h"
 #include "TString.h"
+#include "LeptonIDEnums.h"
 #include "ViewColumns.h"
 
 /**
@@ -64,48 +65,8 @@ struct MuonSoA {
 
 class MuonView {
 public:
-    enum class WorkingPoint : unsigned char {
-        NONE = 0,
-        VLOOSE = 1,
-        LOOSE = 2,
-        MEDIUM = 3,
-        TIGHT = 4,
-        VTIGHT = 5,
-        VVTIGHT = 6
-    };
-
-    enum class MuonID {
-        NOCUT,
-        POG_TIGHT,
-        POG_MEDIUM,
-        POG_MEDIUM_PROMPT,
-        POG_LOOSE,
-        POG_SOFT,
-        POG_SOFT_MVA,
-        POG_TRIGGER_LOOSE,
-        POG_TRACKER_HIGH_PT,
-        POG_GLOBAL_HIGH_PT,
-        POG_MINISO_LOOSE,
-        POG_MINISO_MEDIUM,
-        POG_MINISO_TIGHT,
-        POG_MINISO_VTIGHT,
-        POG_MULTISO_LOOSE,
-        POG_MULTISO_MEDIUM,
-        POG_MVA_MU_MEDIUM,
-        POG_MVA_MU_TIGHT,
-        POG_PFISO_VLOOSE,
-        POG_PFISO_LOOSE,
-        POG_PFISO_MEDIUM,
-        POG_PFISO_TIGHT,
-        POG_PFISO_VTIGHT,
-        POG_PFISO_VVTIGHT,
-        POG_PUPPIISO_LOOSE,
-        POG_PUPPIISO_MEDIUM,
-        POG_PUPPIISO_TIGHT,
-        POG_TKISO_LOOSE,
-        POG_TKISO_TIGHT,
-        POG_PROMPTMVA_WP0p64
-    };
+    using WorkingPoint = LeptonID::MuonWorkingPoint;
+    using MuonID = LeptonID::MuonID;
 
     MuonView() = default;
     MuonView(std::shared_ptr<const MuonSoA> data, std::size_t index)

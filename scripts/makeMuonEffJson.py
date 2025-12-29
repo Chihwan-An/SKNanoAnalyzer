0d395df0c6502caa4b13d4816bf6771f65688dbb
Yeonjoon <yeonjoon.kim@cern.ch>
2025-12-29 16:37:22 +0900

    add 2024 muo

--- scripts/makeMuonEffJson.py
+++ scripts/makeMuonEffJson.py
@@ -6,13 +6,14 @@ import argparse
 import os
 import itertools
 import copy
+import sys
 
 def remove_supersets(strings):
     result = []
     for s in strings:
         if any(s != other and s in other for other in strings):
             result.append(s)
-    return result
+    return result if result else strings
 
 def histParser(f, target_binning):
     hist_dicts = {}
@@ -36,16 +37,9 @@ def histParser(f, target_binning):
         hist_dicts[trig]['effMC_syst'] = trig+"_"+target_binning+"_efficiencyMC_syst"
     return hist_dicts 
         
-def makeTempHist(hist_dicts, era):
-    folder = os.path.join(os.environ['SKNANO_DATA'],era,'MUO')
-    older = os.path.join(os.environ['SKNANO_DATA'],era,'MUO')
-    hist_root = os.listdir(folder)
-    hist_root = [x for x in hist_root if 'SingleMuonTriggers.root' in x]
-    if len(hist_root) == 0:
-        return
-    hist_root = os.path.join(folder,hist_root[0])
-    f = ROOT.TFile(hist_root,"READ")
-    f_out = ROOT.TFile("output.root","RECREATE")
+def makeTempHist(hist_dicts, hist_root, tmp_root_path):
+    f = ROOT.TFile(hist_root, "READ")
+    f_out = ROOT.TFile(tmp_root_path, "RECREATE")
     for trig, hist_dict in hist_dicts.items():
         
         hist_effData = f.Get(hist_dict['effData'])
@@ -83,15 +77,20 @@ def makeTempHist(hist_dicts, era):
     f_out.Close()
     f.Close()
       
-def makingJson(era, target_binning):
-    folder = os.path.join(os.environ['SKNANO_DATA'],era,'MUO')
-    hist_root = os.listdir(folder)
-    hist_root = [x for x in hist_root if 'SingleMuonTriggers.root' in x]
-    if len(hist_root) == 0:
-        return
-    hist_root = os.path.join(folder,hist_root[0])
-    f = ROOT.TFile(hist_root,"READ")
+def _binning_inputs(target_binning):
+    if target_binning == 'abseta_pt':
+        return ('abseta', 'pt')
+    if target_binning == 'eta_pt':
+        return ('eta', 'pt')
+    return None
+
+def makingJson(hist_root, target_binning, tmp_root_path):
+    f = ROOT.TFile(hist_root, "READ")
     hist_dicts = histParser(f, target_binning)
+    if len(hist_dicts) == 0:
+        print(f"No histograms found for binning '{target_binning}' in {hist_root}")
+        f.Close()
+        return None
     
     trigs = []
     main_json = {"schema_version": 2,
@@ -101,20 +100,24 @@ def makingJson(era, target_binning):
         
     }
     
-    if target_binning == 'abseta_pt':
-        categorys['data_or_mc'] = {"type":"string","description":"data or mc","content":["data","mc"]}
-        categorys['scale_factors'] = {"type":"string","description":"eff,effup,effdown","content":[]}
-        categorys['abseta'] = {"type":"real","description":"abseta"}
-        categorys['pt'] = {"type":"real","description":"pt"}
-        
-    else:
+    axis_inputs = _binning_inputs(target_binning)
+    if axis_inputs is None:
         print("If you want to use different binning, please modify the code")
+        f.Close()
+        return None
 
-    makeTempHist(histParser(f, target_binning), era)
+    axis_x, axis_y = axis_inputs
+    categorys['data_or_mc'] = {"type":"string","description":"data or mc","content":["data","mc"]}
+    categorys['scale_factors'] = {"type":"string","description":"eff,effup,effdown","content":[]}
+    categorys[axis_x] = {"type":"real","description":axis_x}
+    categorys[axis_y] = {"type":"real","description":axis_y}
+
+    makeTempHist(hist_dicts, hist_root, tmp_root_path)
+    f.Close()
 
     trigs = hist_dicts.keys()
     
-    categorys['scale_factors']['content'] = ['effData','effDataUp','effDataDown','effMC','effMCUp','effMCDown']
+    categorys['scale_factors']['content'] = ['nominal','systup','systdown']
     
     for trig in trigs:
         trig_eff_dict={}
@@ -132,12 +135,12 @@ def makingJson(era, target_binning):
         
         eff_dict = {}
         
-        eff_dict['effdata'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyData')
-        eff_dict['effdataup'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyDataUp')
-        eff_dict['effdatadown'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyDataDown')
-        eff_dict['effmc'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyMC')
-        eff_dict['effmcup'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyMCUp')
-        eff_dict['effmcdown'] = convert.from_uproot_THx(f'output.root:{trig}_{target_binning}_efficiencyMCDown')
+        eff_dict['effdata'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyData')
+        eff_dict['effdataup'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyDataUp')
+        eff_dict['effdatadown'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyDataDown')
+        eff_dict['effmc'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyMC')
+        eff_dict['effmcup'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyMCUp')
+        eff_dict['effmcdown'] = convert.from_uproot_THx(f'{tmp_root_path}:{trig}_{target_binning}_efficiencyMCDown')
         
         trig_eff_dict['data'] = {"nodetype": "category",
                                     "input": "data_or_mc",
@@ -146,21 +149,21 @@ def makingJson(era, target_binning):
         
         for key, eff in eff_dict.items():
             eff = eff.dict()['data']
-            eff['inputs'] = ['abseta','pt']
+            eff['inputs'] = [axis_x, axis_y]
             eff['flow'] = 'clamp'
             if 'effdata' in key:
                 this_key = 'nominal'
                 if 'up' in key:
-                    this_key = 'sfup'
+                    this_key = 'systup'
                 elif 'down' in key:
-                    this_key = 'sfdown'
+                    this_key = 'systdown'
                 trig_eff_dict['data']['content'][0]['value']['content'].append({"key":this_key,"value":eff})
             else:
                 this_key = 'nominal'
                 if 'up' in key:
-                    this_key = 'sfup'
+                    this_key = 'systup'
                 elif 'down' in key:
-                    this_key = 'sfdown'
+                    this_key = 'systdown'
                 trig_eff_dict['data']['content'][1]['value']['content'].append({"key":this_key,"value":eff})
                 
         
@@ -180,20 +183,45 @@ def makingJson(era, target_binning):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Create JSON file for b-tagging efficiency')
     parser.add_argument('--out_name_str',dest='out_name_str', help='Output JSON file name', default='')
+    parser.add_argument('--input_root',dest='input_root', help='Input ROOT file (bypass SKNANO_DATA lookup)', default='')
+    parser.add_argument('--out_dir',dest='out_dir', help='Output directory for JSON', default='')
+    parser.add_argument('--out_json',dest='out_json', help='Output JSON path (overrides out_dir)', default='')
+    parser.add_argument('--target_binning',dest='target_binning', help='Binning name in histogram keys', default='abseta_pt')
+    parser.add_argument('--tmp_root',dest='tmp_root', help='Temporary ROOT file path', default='output.root')
+    parser.add_argument('--eras',dest='eras', help='Comma-separated eras (legacy mode)', default='')
     args = parser.parse_args()
     out_name_str = args.out_name_str
 
+    if args.input_root:
+        hist_root = args.input_root
+        target_binning = args.target_binning
+        main_json = makingJson(hist_root, target_binning, args.tmp_root)
+        if main_json is None:
+            sys.exit(1)
+        if args.out_json:
+            out_path = args.out_json
+        else:
+            out_dir = args.out_dir if args.out_dir else os.getcwd()
+            out_path = os.path.join(out_dir, out_name_str + 'muon_trig.json')
+        with open(out_path, 'w') as f:
+            json.dump(main_json, f, indent=4)
+        sys.exit(0)
+
     totalEras = ['2022','2022EE']
     totalEras = ['2017','2016preVFP','2016postVFP','2018']
+    if args.eras:
+        totalEras = [x.strip() for x in args.eras.split(',') if x.strip()]
     for era in totalEras:
-        out_dir = os.path.join(os.environ['SKNANO_DATA'],era,'MUO')
-        target_binning = 'abseta_pt'
-        main_json = makingJson(era,target_binning)
-        
-        with open(os.path.join(out_dir,out_name_str+'muon_trig.json'), 'w') as f:
+        out_dir = args.out_dir if args.out_dir else os.path.join(os.environ['SKNANO_DATA'], era, 'MUO')
+        hist_root = os.path.join(out_dir, 'SingleMuonTriggers.root')
+        if not os.path.exists(hist_root):
+            hist_roots = [x for x in os.listdir(out_dir) if 'SingleMuonTriggers.root' in x]
+            if len(hist_roots) == 0:
+                continue
+            hist_root = os.path.join(out_dir, hist_roots[0])
+        main_json = makingJson(hist_root, args.target_binning, args.tmp_root)
+        if main_json is None:
+            continue
+        with open(os.path.join(out_dir, out_name_str+'muon_trig.json'), 'w') as f:
             json.dump(main_json, f, indent=4)
-
-
-
-
 
